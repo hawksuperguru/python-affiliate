cf2f8b741363090c0572eb7ed95c536d20d5e26e
kapphire <ken.sapphire87@yahoo.com>
2017-07-31 17:18:04 +0200

    netbet.py updated

--- scrapping/netbet.py
+++ scrapping/netbet.py
@@ -40,6 +40,7 @@ def netbet_scrapping():
             # tmp = pattern.search(balance)
             # balance = tmp.group(0)
             # print(balance)
+            time.sleep(4)
             header = Netbet.find_element_by_class_name('dshb_icoNav')
             statistics = header.find_elements_by_tag_name('li')[2]
             statistics.click()
