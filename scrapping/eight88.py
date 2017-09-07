8d74f10494edaf96564cd773a2072e0b93da93bc
Dev Ops <wealthy.gurub@yahoo.com>
2017-09-07 17:37:29 +0200

    eight88 Spider updated

--- scrapping/eight88.py
+++ scrapping/eight88.py
@@ -1,86 +1,156 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait as wait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
-from pyvirtualdisplay import Display
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from selenium_browser import UBrowse
 from sqlalchemy import create_engine
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import ( Select, WebDriverWait )
+from selenium.webdriver.common.keys import Keys
+
+from settings.config import *
+
 import psycopg2
-import os, time
+import datetime
+import json
+import requests
+import time
+import re
+
+class Eight88(object):
+    """docstring for Eight88"""
+    def __init__(self):
+        self.client = UBrowse()
+        self.login_url = 'http://affiliates.888.com/'
+        self.report_url = 'https://affiliate.coral.co.uk/reporting/quick_summary_report.asp'
+        self.username = 'betfyuk1'
+        self.password = 'dontfuckwithme'
+        self.items = []
+        self.timer = 0
+
+        self.headers = {
+            'Host': 'secure.activewins.com',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Referer': 'https://secure.activewins.com/reporting/quick_summary_report.asp',
+        }
+
+    def _create_params(self, from_date, to_date, media=False):
+        if media:
+            self.params = (
+                ('key', 'value'),
+                )
+        else:
+            self.params = (
+                ('operatorID', '1'),
+                )
+
+    def _get_cookies(self):
+        self.cookies = dict()
+        cookies = self.client.driver.get_cookies()
+        for i in cookies:
+            self.cookies[i['name']] = i['value']
+
+    def get_delta_date(self, delta = 1, format_string = "%Y/%m/%d"):
+        today = datetime.datetime.today()
+        diff = datetime.timedelta(days = delta)
+        return (today - diff).strftime(format_string)
+
+    def close(self):
+        self.client.close()
+
+    def login(self, username = 'betfyuk', password = 'LALB37hUhs'):
+        try:
+            self.client.driver.find_element_by_class_name("hide-under-480").click()
+            WebDriverWait(self.client.driver, 10).until(expected_conditions.frame_to_be_available_and_switch_to_it(self.client.driver.find_element_by_xpath('//iframe[contains(@src, "Auth/Login")]')))
+            self.client.driver.find_element_by_id("userName").send_keys("betfyuk")
+            self.client.driver.find_element_by_id("password").send_keys("LALB37hUhs")
+            self.client.driver.find_element_by_id("btnLogin").click()
+            time.sleep(1)
+            self._get_cookies()
+            return True
+        except:
+            return False
+
+    def parse_page(self):
+        try:
+            self.client.driver.find_element_by_id("rbQuickStatID_This Month (1st - Today)").click()
+            bal = self.client.driver.find_element_by_id("this-month").text
+            balCents = self.client.driver.find_element_by_id("this-month-cents").text
+            netBal = bal + balCents
+            prebal = self.client.driver.find_element_by_id("last-month").text
+            for summarise in self.client.driver.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
+                self.items.append(summarise.text)
+            self.items.append(netBal)
+
+            self.client.driver.find_element_by_id("rbQuickStatID_Last 7 Days").click()
+            time.sleep(2)
+            for summarise in self.client.driver.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
+                self.items.append(summarise.text)
+
+            self.client.driver.find_element_by_id("rbQuickStatID_Previous Month").click()
+            time.sleep(2)
+            for summarise in self.client.driver.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
+                self.items.append(summarise.text)
+
+            self.client.driver.find_element_by_id("rbQuickStatID_Today").click()
+            time.sleep(2)
+            for summarise in self.client.driver.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
+                self.items.append(summarise.text)
+            self.items.append(prebal)
+            return True
+        except:
+            print("An error occured in parsing page...")
+            return False
+
+    def save(self):
+        impression = int(self.items[0])
+        click = int(self.items[1])
+        registration = int(self.items[2])
+        lead = int(self.items[3])
+        money_player = int(self.items[4])
+        balance = float(self.items[5])
+        imprwk = int(self.items[6])
+        cliwk = int(self.items[7])
+        regwk = int(self.items[8])
+        leadwk = int(self.items[9])
+        mpwk = int(self.items[10])
+        imprpre = int(self.items[11])
+        clipre = int(self.items[12])
+        regpre = int(self.items[13])
+        leadpre = int(self.items[14])
+        mppre = int(self.items[15])
+        imprto = int(self.items[16])
+        clito = int(self.items[17])
+        regto = int(self.items[18])
+        leadto = int(self.items[19])
+        mpto = int(self.items[20])
+        prebal = int(self.items[21])
+        dateto = self.get_delta_date()
+
+        engine = create_engine(get_database_connection_string())
+        result = engine.execute("INSERT INTO eight88s (impression, click, registration, lead, money_player, balance, imprwk, cliwk, regwk, leadwk, mpwk, imprpre, clipre, regpre, leadpre, mppre, imprto, clito, regto, leadto, mpto, prebalance, dateto) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", impression, click, registration, lead, money_player, balance, imprwk, cliwk, regwk, leadwk, mpwk, imprpre, clipre, regpre, leadpre, mppre, imprto, clito, regto, leadto, mpto, prebal, dateto)
+
+    def run(self):
+        self.client.open_url(self.login_url)
+        time.sleep(1)
+
+        print("Logging in....")
+        if self.login() is True:
+            print("Successfully logged in.")
+            if self.parse_page() is True:
+                self.save()
+            else:
+                print("Parsing Error...")
+
+        else:
+            print("Failed to log in.")
+
+        self.close()
 
 
-def eight88_scrapping():
-
-	display = Display(visible = 0, size = (1200, 900))
-	display.start()
-	try:
-		eight88 = webdriver.Chrome(executable_path=os.path.abspath("/usr/bin/chromedriver"))
-		eight88.set_window_size(1120, 550)
-		eight88.get("http://affiliates.888.com/")
-		assert "Earn Real Money" in eight88.title
-		eight88.find_element_by_class_name("hide-under-480").click()
-		wait(eight88, 10).until(EC.frame_to_be_available_and_switch_to_it(eight88.find_element_by_xpath('//iframe[contains(@src, "Auth/Login")]')))
-		eight88.find_element_by_id("userName").send_keys("betfyuk")
-		eight88.find_element_by_id("password").send_keys("LALB37hUhs")
-		eight88.find_element_by_id("btnLogin").click()
-		balance_arr = []
-		eight88.find_element_by_id("rbQuickStatID_This Month (1st - Today)").click()
-		bal = eight88.find_element_by_id("this-month").text
-		balCents = eight88.find_element_by_id("this-month-cents").text
-		netBal = bal + balCents
-		prebal = eight88.find_element_by_id("last-month").text
-		for summarise in eight88.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
-		    balance_arr.append(summarise.text)
-		balance_arr.append(netBal)
-
-		eight88.find_element_by_id("rbQuickStatID_Last 7 Days").click()
-		time.sleep(2)
-		for summarise in eight88.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
-			balance_arr.append(summarise.text)
-
-		eight88.find_element_by_id("rbQuickStatID_Previous Month").click()
-		time.sleep(2)
-		for summarise in eight88.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
-			balance_arr.append(summarise.text)
-
-		eight88.find_element_by_id("rbQuickStatID_Today").click()
-		time.sleep(2)
-		for summarise in eight88.find_elements_by_xpath('.//span[@class = "summariseTab"]'):
-			balance_arr.append(summarise.text)
-		balance_arr.append(prebal)
-
-		return balance_arr
-		
-	finally:
-		eight88.quit()
-		display.stop()
-
-data = eight88_scrapping()
-
-impression = int(data[0])
-click = int(data[1])
-registration = int(data[2])
-lead = int(data[3])
-money_player = int(data[4])
-balance = float(data[5])
-imprwk = int(data[6])
-cliwk = int(data[7])
-regwk = int(data[8])
-leadwk = int(data[9])
-mpwk = int(data[10])
-imprpre = int(data[11])
-clipre = int(data[12])
-regpre = int(data[13])
-leadpre = int(data[14])
-mppre = int(data[15])
-imprto = int(data[16])
-clito = int(data[17])
-regto = int(data[18])
-leadto = int(data[19])
-mpto = int(data[20])
-prebal = int(data[21])
-
-engine = create_engine('postgresql://postgres:root@localhost/kyan')
-result = engine.execute("INSERT INTO eight88s (impression, click, registration, lead, money_player, balance, imprwk, cliwk, regwk, leadwk, mpwk, imprpre, clipre, regpre, leadpre, mppre, imprto, clito, regto, leadto, mpto, prebalance) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", impression, click, registration, lead, money_player, balance, imprwk, cliwk, regwk, leadwk, mpwk, imprpre, clipre, regpre, leadpre, mppre, imprto, clito, regto, leadto, mpto, prebal)
+if __name__ == "__main__":
+    eight88 = Eight88()
+    eight88.run()
