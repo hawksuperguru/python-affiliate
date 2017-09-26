e83da4d6e2c46e03a774e83e329be2f45cc32ad5
Alexis Richard <ecmascript.guru@gmail.com>
2017-09-26 14:19:02 -0400

    internal error handler created

--- app/home/views.py
+++ app/home/views.py
@@ -141,3 +141,11 @@ def api_histories():
     histories = get_histories(mode, date_range)
 
     return jsonify(status = True, data = histories)
+
+@home.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
